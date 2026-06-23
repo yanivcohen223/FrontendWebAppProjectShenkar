7d3593daadb867c1287b0d909a8c955de8dc976b
Eilon Greenberg <eilon343@gmail.com>
2026-06-23 11:11:16 +0300

    added prod_api url

--- js/shared/config.js
+++ js/shared/config.js
@@ -1,6 +1,6 @@
 
 const LOCAL_API = 'http://localhost:3000/api';
-const PROD_API = 'https://DEPLOYED-BACKEND/api'; 
+const PROD_API = 'https://sportie-server.onrender.com/api'; 
 
 const isLocal =
     location.hostname === 'localhost' || location.hostname === '127.0.0.1';
