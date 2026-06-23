5ce0f5af403ebc2c985850813b7a871f84551d33
yanivcohen223 <yanivcohen223@gmail.com>
2026-06-23 12:56:58 +0300

    improve the ui top bar

--- js/shared/topbar.js
+++ js/shared/topbar.js
@@ -2,15 +2,11 @@ function buildUserAreaHTML() {
     const raw = sessionStorage.getItem('sportieSession');
     const trainerName = raw ? (JSON.parse(raw)?.trainer?.name || '') : '';
     return `
-    <button class="bell-btn" type="button" aria-label="Notifications">
-        <img src="images/notificationIcon.svg" alt="Notifications" width="24" height="24">
-    </button>
     <div class="user-area">
         <div class="user-avatar">
             <img class="user-avatar-icon" src="images/profileIcon.png" alt="" width="14" height="14">
         </div>
         <span class="user-name">${trainerName}</span>
-        <span class="user-chevron">&#9660;</span>
     </div>
 `;
 }
